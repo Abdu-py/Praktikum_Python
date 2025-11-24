29ffa79c31654e2a644bf1e4cecc82e65b0079a1
Muhammad Abdu <abdudisini@gmail.com>
2025-11-24 13:38:32 +0700

    Add Python operator examples and report
    Added a Python script and Jupyter notebook demonstrating various operator types (arithmetic, comparison, logical, bitwise, assignment, identity, and membership) for practical and documentation purposes in AI programming coursework.

---
+++ coding/praktikum9_operator-python.py
@@ -0,0 +1,42 @@
+# Operator aritmatika
+x = 10
+y = 3
+print("Tambah:", x+y)
+print("Bagi:", x/y)
+print("Modulus:", x%y)
+print("Kali:", x*y)
+print("Floor:", x//y)
+
+# Operator perbandingan
+x = 10
+y = 5
+print(x == y)
+print(x > y)
+print(x < y)
+
+# Operator logika
+x = 10
+y = 5
+print((x>y) and (y>0))
+print((x<y) or (y>0))
+print(not(x>y))
+
+# Operator bitwise
+x = 5
+y = 3
+print(x & y)
+print(x | y)
+print(x ^ y)
+
+# Operator penugasan
+x = 5
+x += 3
+x *= 2
+x /= 4
+print(x)
+
+# Operator identitas dan keanggotaan
+x = [1,2,3]
+y = x
+print(x is y)
+print(2 in x)
